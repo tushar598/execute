8204879c910209f1b6e9bd2c23b651bf2ba37b9c
Sheetal2216 <sheetalpandey2205@gmail.com>
2026-03-01 15:52:39 +0530

    feat (sheetal) : sun tokenmarketlisting.ts model done

--- app/models/suntokenmarketlisting.ts
+++ app/models/suntokenmarketlisting.ts
@@ -16,33 +16,33 @@ export interface ISunTokenMarketListing extends Document {
     updatedAt: Date;
 }
 
-// const SunTokenMarketListingSchema: Schema<ISunTokenMarketListing> = new Schema(
-//     {
-//         sellerId: {
-//             type: Schema.Types.ObjectId,
-//             ref: 'User',
-//             required: true,
-//         },
-//         sellerName: { type: String, required: true, trim: true },
-//         tokens: { type: Number, required: true, min: 1 },
-//         pricePerToken: { type: Number, required: true },
-//         totalValue: { type: Number, required: true },
-//         location: {
-//             lat: { type: Number, required: true },
-//             lng: { type: Number, required: true },
-//         },
-//         state: { type: String, required: true, trim: true },
-//         status: {
-//             type: String,
-//             enum: ['available', 'sold'],
-//             default: 'available',
-//         },
-//     },
-//     { timestamps: true }
-// );
+const SunTokenMarketListingSchema: Schema<ISunTokenMarketListing> = new Schema(
+    {
+        sellerId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        sellerName: { type: String, required: true, trim: true },
+        tokens: { type: Number, required: true, min: 1 },
+        pricePerToken: { type: Number, required: true },
+        totalValue: { type: Number, required: true },
+        location: {
+            lat: { type: Number, required: true },
+            lng: { type: Number, required: true },
+        },
+        state: { type: String, required: true, trim: true },
+        status: {
+            type: String,
+            enum: ['available', 'sold'],
+            default: 'available',
+        },
+    },
+    { timestamps: true }
+);
 
-// const SunTokenMarketListing: Model<ISunTokenMarketListing> =
-//     mongoose.models.SunTokenMarketListing ||
-//     mongoose.model<ISunTokenMarketListing>('SunTokenMarketListing', SunTokenMarketListingSchema);
+const SunTokenMarketListing: Model<ISunTokenMarketListing> =
+    mongoose.models.SunTokenMarketListing ||
+    mongoose.model<ISunTokenMarketListing>('SunTokenMarketListing', SunTokenMarketListingSchema);
 
-// export default SunTokenMarketListing;
+export default SunTokenMarketListing;
